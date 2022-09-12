e474d0a9daa528cdd0b50176d30a3e57c79c864a
eunseoryu <22100466@handong.ac.kr>
2022-09-12 22:33:12 +0900

    add nextLine

--- src/main/java/org/example/WordCRUD.java
+++ src/main/java/org/example/WordCRUD.java
@@ -73,6 +73,7 @@ public class WordCRUD implements ICRUD{
         ArrayList<Integer> idlist = this.listAll(keyword);
         System.out.print("=> 수정할 번호 선택: ");
         int id = s.nextInt();
+        s.nextLine();
         System.out.print("=> 뜻 입력 : ");
         String meaning = s.nextLine();
         Word word = list.get(idlist.get(id-1));
